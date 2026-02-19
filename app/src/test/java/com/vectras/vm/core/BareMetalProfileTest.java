643bb127088df27f8718de69df269116270d2daf
Rafael mreis <rafaelmeloreisnovo@gmail.com>
2026-02-19 17:16:02 -0300

    Add mapArch signature mapping regression tests

--- app/src/test/java/com/vectras/vm/core/BareMetalProfileTest.java
+++ app/src/test/java/com/vectras/vm/core/BareMetalProfileTest.java
@@ -2,6 +2,9 @@ package com.vectras.vm.core;
 
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import static org.junit.Assert.*;
 
 public class BareMetalProfileTest {
@@ -11,6 +14,35 @@ public class BareMetalProfileTest {
         int arch = BareMetalProfile.detectArchitecture();
         assertTrue(arch >= BareMetalProfile.ARCH_UNKNOWN);
         assertTrue(arch <= BareMetalProfile.ARCH_RISCV64);
+    }
+
+    @Test
+    public void testMapArchSignatureMapping() {
+        assertEquals(BareMetalProfile.ARCH_ARM64, mapArchForTest(NativeFastPath.ARCH_ARM64));
+        assertEquals(BareMetalProfile.ARCH_ARM32, mapArchForTest(NativeFastPath.ARCH_ARM32));
+        assertEquals(BareMetalProfile.ARCH_X86_64, mapArchForTest(NativeFastPath.ARCH_X64));
+        assertEquals(BareMetalProfile.ARCH_X86, mapArchForTest(NativeFastPath.ARCH_X86));
+        assertEquals(BareMetalProfile.ARCH_RISCV64, mapArchForTest(NativeFastPath.ARCH_RISCV64));
+        assertEquals(BareMetalProfile.ARCH_UNKNOWN, mapArchForTest(0x7F00));
+    }
+
+    @Test
+    public void testMapArchIgnoresUnrelatedExtraSignatureBits() {
+        int signatureWithExtras = NativeFastPath.ARCH_ARM64
+                | NativeFastPath.FEATURE_AES
+                | NativeFastPath.FEATURE_SIMD
+                | 0xAB000000
+                | NativeFastPath.OS_ANDROID;
+        assertEquals(BareMetalProfile.ARCH_ARM64, mapArchForTest(signatureWithExtras));
+    }
+
+    @Test
+    public void testMapArchDoesNotInferArchitectureFromFeatureBits() {
+        int unknownWithoutFeatures = 0x0000;
+        int unknownWithFeatures = NativeFastPath.FEATURE_AES | NativeFastPath.FEATURE_SIMD | NativeFastPath.FEATURE_CRC32;
+
+        assertEquals(BareMetalProfile.ARCH_UNKNOWN, mapArchForTest(unknownWithoutFeatures));
+        assertEquals(BareMetalProfile.ARCH_UNKNOWN, mapArchForTest(unknownWithFeatures));
     }
 
     @Test
@@ -40,5 +72,17 @@ public class BareMetalProfileTest {
                 | BareMetalProfile.CAP_AES
                 | BareMetalProfile.CAP_CRC32;
         assertEquals(0, flags & ~knownMask);
+    }
+
+    private static int mapArchForTest(int signature) {
+        try {
+            Method method = BareMetalProfile.class.getDeclaredMethod("mapArch", int.class);
+            method.setAccessible(true);
+            return (Integer) method.invoke(null, signature);
+        } catch (NoSuchMethodException | IllegalAccessException e) {
+            throw new AssertionError("Unable to invoke mapArch(int signature)", e);
+        } catch (InvocationTargetException e) {
+            throw new AssertionError("mapArch(int signature) threw an exception", e.getCause());
+        }
     }
 }
